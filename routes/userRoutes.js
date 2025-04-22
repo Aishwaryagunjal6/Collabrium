630ddef6feffc42894547569d2677ddc2c58acae
Aishwaryagunjal6 <gunjalaishwarya625@gmail.com>
2025-04-22 10:53:58 +0530

    Added user registration logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,27 @@ userRouter.post("/register", async (req, res)=>{
     const { username, email, password } = req.body()
     //check if user already exists
     const userExists = await User.findOne({email})
-  }catch(error){
+    if(userExists){
+      return res.status(400).json({
+        message: "User Already exists"
+      })
+    }
 
+    //create new user
+    const user = await User.create({
+      username, email, password
+    })
+
+    if(user){
+      res.status(201).json({
+        _id : user._id,
+        username: user.username,
+        email: user.email,
+      })
+    }
+  }catch(error){
+    res.status(400).json({
+      message: error.message
+    })
   }
 })
