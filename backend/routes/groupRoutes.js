7af8abfe64773395a2401c04bb125d0b38b82f2d
Aishwaryagunjal6 <gunjalaishwarya625@gmail.com>
2025-04-25 19:07:51 +0530

    Initial commit with frontend and backend

---
+++ backend/routes/groupRoutes.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const Group = require("../models/groupModel.js");
+const {protect , isAdmin}= require("../middlewares/authMiddleware.js");
+const groupRouter = express.Router();
+
+//Create a new group 
+groupRouter.post("/", protect , isAdmin ,async(req, res)=>{
+  try{
+    const {name, description} = req.body;
+    const group = await Group.create({
+      name,
+      description,
+      admin: req.user._id,
+      members:[req.user._id]
+    })
+
+    const populatedGroup = await Group.findById(group._id).populate("admin", "username email").populate('members', 'username email')
+
+    res.status(201).json({populatedGroup})
+  }catch(error){
+    res.staus(400).json({
+      message: error.message
+    })
+  }
+})
+
+// get all groups
+groupRouter.get("/", protect , async (req, res)=>{
+  try{
+    const groups = await Group.find().populate("admin" , "username email").populate("members" , "username email");
+    res.json(groups)
+  }catch(error){
+    res.status(400).json({
+      message: error.message
+    })
+  }
+})
+
+//Join Group
+groupRouter.post("/:groupId/join", protect , async(req, res)=>{
+  try{
+    const group = await Group.findById(req.params.groupId)
+    // console.log(group)
+  
+    if(!group){
+      return res.status(404).json({
+        message: "Group not found"
+      })
+    }
+    if(group.members.includes(req.user._id)){
+      return res.status(400).json({
+        message: "Already a member of this group"
+      })
+    }
+    group.members.push(req.user._id);
+    await group.save()
+    res.json({
+      message: "Successfully joined the group"
+    })
+  }catch(error){
+    res.status(400).json({
+      message: error.message
+    })
+  }
+})
+
+module.exports = groupRouter;
